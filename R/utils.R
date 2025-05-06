ce8a13e6d45ddfdbc2cea4ac213387418f4d68f1
Chris Pritchard <chris@christopherpritchard.co.uk>
2025-05-06 16:11:41 +0100

    Start work on sane defaults if missing from CSV

--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,30 @@
 # Utility functions for PRISMA_flowdiagram
 
+#' Pull from CSV, or use a default value if not in the CSV
+#' @description set the variable from the CSV, or a default value
+#' @param expr the expression to get the variable from the CSV
+#' @param var_name the variable name
+#' @param default the default token
+#' @return the result of 'expr', or 'default'
+#' @keywords internal
+PRISMA_default_or_csv_ <- function (expr, default, var_name = "not provided") { #nolint
+  result <- expr
+  if (length(result) == 0) {
+    warning(
+      c(
+        "Variable ",
+        var_name,
+        " was not processed because it does",
+        " not appear to exist within the CSV file",
+        " using default value: ",
+        default
+      )
+    )
+    return(default)
+  }
+  return(result)
+}
+
 #' Calculate the correct height of a box from a list (e.g. of exclusion reasons)
 #' @description Get the correct height for a box
 #' @param n the number of rows of text in the label
