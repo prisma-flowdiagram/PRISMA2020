b502af2347e8e6e42f39e9ba69b3f72a75fb46d2
Chris Pritchard <chris@christopherpritchard.co.uk>
2026-06-04 12:31:04 +0200

    split functions to seperate files

---
+++ R/internal/PRISMA_calc_filetype.R
@@ -0,0 +1,20 @@
+#' Calculate the correct filetime
+#'
+#' @description Work out the correct filetype to save the file as
+#' @param fn The filename (including extension)
+#' @param ft The filetype (which can be NA or NULL)
+#' @return the filetype taken from the filename, or overriden by the ft param
+#' @keywords internal
+PRISMA_calc_filetype_ <- function(fn, ft) { #nolint
+    # if the filetype is set, return that, otherwise
+    # calculate the filetype from the extension (HTM becomes HTML)
+    if (!is.na(ft) && !is.null(ft)) {
+      the_ft <- toupper(ft)
+    } else {
+      the_ft <- toupper(tools::file_ext(fn))
+      if (the_ft == "HTM") {
+        the_ft <- "HTML"
+      }
+    }
+    return(the_ft)
+}
---
+++ R/internal/PRISMA_default_or_csv.R
@@ -0,0 +1,24 @@
+#' Pull from CSV, or use a default value if not in the CSV
+#' @description set the variable from the CSV, or a default value
+#' @param expr the expression to get the variable from the CSV
+#' @param var_name the variable name
+#' @param default the default token
+#' @return the result of 'expr', or 'default'
+#' @keywords internal
+PRISMA_default_or_csv_ <- function (expr, default, var_name = "not provided") { #nolint
+  result <- expr
+  if (length(result) == 0) {
+    warning(
+      c(
+        "Variable ",
+        var_name,
+        " was not processed because it does",
+        " not appear to exist within the CSV file",
+        " using default value: ",
+        default
+      )
+    )
+    return(default)
+  }
+  return(result)
+}
